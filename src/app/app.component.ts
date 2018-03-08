b1b16c3a13a1e1449f70463c7dd952ef2e185739
deniz946 <deniz946@gmail.com>
2018-03-08 20:39:47 +0100

    Created basic component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  public newTodo: any = '';
+  public todos: any = [{ title: 'mi todo', done: true },
+  { title: 'mi todo2', done: false }];
+
+  deleteTodo(index: number) {
+    console.log(index);
+    this.todos.splice(index, 1);
+  }
+
+  addTodo(todo) {
+    this.todos.unshift(this.generateNewTodo(todo));
+    this.newTodo = '';
+  }
+
+  generateNewTodo(todo): any {
+    return { title:todo, done: false };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -7,10 +8,11 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
